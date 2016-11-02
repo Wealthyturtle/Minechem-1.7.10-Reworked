7bc5e23325f15ab73cc84aa8f047c4b350677eb0
Wealthyturtle <limbennett@yahoo.com.sg>
2016-11-02 13:31:07 +0800

    Cleanup

--- src/main/java/wealthyturtle/uiesingularities/proxy/ClientProxy.java
+++ src/main/java/wealthyturtle/uiesingularities/proxy/ClientProxy.java
@@ -1,13 +1,9 @@
 package wealthyturtle.uiesingularities.proxy;
 
-import org.lwjgl.opengl.GL11;
-
-import fox.spiteful.avaritia.items.LudicrousItems;
 import fox.spiteful.avaritia.render.FancyHaloRenderer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.IItemRenderer;
 import net.minecraftforge.client.MinecraftForgeClient;
-import wealthyturtle.uiesingularities.EternalSingularityItem;
 
 public final class ClientProxy extends CommonProxy
 {
@@ -16,6 +12,5 @@ public final class ClientProxy extends CommonProxy
 	{
 		final FancyHaloRenderer fancyHaloRenderer = new FancyHaloRenderer();
 		singularities.forEach(singularity -> MinecraftForgeClient.registerItemRenderer(singularity, fancyHaloRenderer));
-		MinecraftForgeClient.registerItemRenderer(eternalSingularity, fancyHaloRenderer);
 	}
 }
