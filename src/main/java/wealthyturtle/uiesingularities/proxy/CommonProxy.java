70b2f0d9f6f7267b71b800aa534659aef11c3f1a
Wealthyturtle <limbennett@yahoo.com.sg>
2016-11-12 23:44:45 +0800

    Version 6 Commit

--- src/main/java/wealthyturtle/uiesingularities/proxy/CommonProxy.java
+++ src/main/java/wealthyturtle/uiesingularities/proxy/CommonProxy.java
@@ -1,14 +1,10 @@
 package wealthyturtle.uiesingularities.proxy;
 
-import cpw.mods.fml.common.Loader;
 import fox.spiteful.avaritia.crafting.CompressorManager;
 import fox.spiteful.avaritia.crafting.Grinder;
-import gnu.trove.list.TIntList;
-import gnu.trove.list.array.TIntArrayList;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.oredict.OreDictionary;
-import wealthyturtle.uiesingularities.NEI;
 import wealthyturtle.uiesingularities.UniversalSingularity;
 import wealthyturtle.uiesingularities.UniversalSingularityItem;
 import wealthyturtle.uiesingularities.UniversalSingularityWrapper;
@@ -25,7 +21,6 @@ import static java.io.File.separatorChar;
 
 public class CommonProxy
 {
-	private final boolean hideDisabledSingularities;
 	protected final List<UniversalSingularityItem> singularities = new ArrayList<>();
 	
 	private final Map<String, Set<String>> allowed = new HashMap<>();
@@ -59,12 +54,11 @@ public class CommonProxy
 						new UniversalSingularityWrapper("osmium", "blockOsmium", 69, 0xAABACE, 0x9BABC4),
 						new UniversalSingularityWrapper("peridot", "blockPeridot", 95, 0x58A52B, 0x4E9226),
 						new UniversalSingularityWrapper("ruby", "blockRuby", 91, 0xB44848, 0x993D3D),
-						new UniversalSingularityWrapper("salt", "blockSalt", 87, 0xEFEFEF, 0xDDE7E8),
 						new UniversalSingularityWrapper("sapphire", "blockSapphire", 86, 0x5173D1, 0x466ACE),
 						new UniversalSingularityWrapper("steel", "blockSteel", 88, 0x9F9F9F, 0x888888),
-						new UniversalSingularityWrapper("titanium", "blockTitanium", 90, 0x1C242E, 0x192029, false),
-						new UniversalSingularityWrapper("tungsten", "blockTungsten", 79, 0xD6D6D6, 0xCACACA, false),
-						new UniversalSingularityWrapper("uranium", "blockUranium", 86, 0x00CB40, 0x00C43E, false),
+						new UniversalSingularityWrapper("titanium", "blockTitanium", 90, 0x1C242E, 0x192029),
+						new UniversalSingularityWrapper("tungsten", "blockTungsten", 79, 0xD6D6D6, 0xCACACA),
+						new UniversalSingularityWrapper("uranium", "blockUranium", 75, 0x00CB40, 0x00C43E),
 						new UniversalSingularityWrapper("zinc", "blockZinc", 76, 0xBFC55C, 0x939836)
 				)),
 				// Big Reactors Singularities
@@ -78,7 +72,7 @@ public class CommonProxy
 				// Draconic Evolution Singularities
 				new UniversalSingularity("draconicEvolution", Arrays.asList(
 						new UniversalSingularityWrapper("draconium", "blockDraconium", 89, 0x8E5CC0, 0x7C4AAE),
-						new UniversalSingularityWrapper("draconiumAwakened", "blockDraconiumAwakened", 83, 0xFF7200, 0xFF6600, false)
+						new UniversalSingularityWrapper("draconiumAwakened", "blockDraconiumAwakened", 83, 0xFF7200, 0xFF6600)
 				)),
 				// Ender IO Singularities
 				new UniversalSingularity("enderIO", Arrays.asList(
@@ -93,12 +87,12 @@ public class CommonProxy
 				)),
 				// ExtraPlanets Singularities
 				new UniversalSingularity("extraPlanets", Arrays.asList(
-						new UniversalSingularityWrapper("blueGem", "blockBlueGem", 95, 0x38D7FF, 0x03C6F6, false),
-						new UniversalSingularityWrapper("carbon", "blockCarbon", 66, 0x1D1D1D, 0x0F0F0F, false),
-						new UniversalSingularityWrapper("crystal", "blockCrystal", 78, 0xE2E2E2, 0xC1C1C1, false),
-						new UniversalSingularityWrapper("palladium", "blockPalladium", 93, 0xF0F0F0, 0xE2E2E2, false),
-						new UniversalSingularityWrapper("redGem", "blockRedGem", 68, 0xFF3B0C, 0xF93100, false),
-						new UniversalSingularityWrapper("whiteGem", "blockWhiteGem", 70, 0xC6C6C6, 0xA5A5A5, false)
+						new UniversalSingularityWrapper("blueGem", "blockBlueGem", 95, 0x38D7FF, 0x03C6F6),
+						new UniversalSingularityWrapper("carbon", "blockCarbon", 66, 0x1D1D1D, 0x0F0F0F),
+						new UniversalSingularityWrapper("crystal", "blockCrystal", 78, 0xE2E2E2, 0xC1C1C1),
+						new UniversalSingularityWrapper("palladium", "blockPalladium", 93, 0xF0F0F0, 0xE2E2E2),
+						new UniversalSingularityWrapper("redGem", "blockRedGem", 68, 0xFF3B0C, 0xF93100),
+						new UniversalSingularityWrapper("whiteGem", "blockWhiteGem", 70, 0xC6C6C6, 0xA5A5A5)
 				)),
 				// ExtraTiC Singularities
 				new UniversalSingularity("extraTiC", Arrays.asList(
@@ -118,21 +112,21 @@ public class CommonProxy
 						new UniversalSingularityWrapper("saltpeter", "blockSaltpeter", 71, 0xF0F0F0, 0xE1E1E1),
 						new UniversalSingularityWrapper("sulfur", "blockSulfur", 66, 0xFFF200, 0xFCD703),
 						//Alloys - Base
-						new UniversalSingularityWrapper("angmallen", "blockAngmallen", 65, 0xE1D78A, 0xD6C761, false),
-						new UniversalSingularityWrapper("damascusSteel", "blockDamascusSteel", 84, 0x996D4D, 0x583F2C, false),
-						new UniversalSingularityWrapper("hepatizon", "blockHepatizon", 82, 0x755E75, 0x614E61, false),
+						new UniversalSingularityWrapper("angmallen", "blockAngmallen", 65, 0xE1D78A, 0xD6C761),
+						new UniversalSingularityWrapper("damascusSteel", "blockDamascusSteel", 84, 0x996D4D, 0x583F2C),
+						new UniversalSingularityWrapper("hepatizon", "blockHepatizon", 82, 0x755E75, 0x614E61),
 						//Alloys - Ender
-						new UniversalSingularityWrapper("desichalkos", "blockDesichalkos", 88, 0x722FA8, 0x502176, false),
+						new UniversalSingularityWrapper("desichalkos", "blockDesichalkos", 88, 0x722FA8, 0x502176),
 						//Alloys - Fantasy
-						new UniversalSingularityWrapper("blackSteel", "blockBlackSteel", 70, 0x395679, 0x314966, false),
-						new UniversalSingularityWrapper("celenegil", "blockCelenegil", 81, 0x94CC48, 0x649128, false),
-						new UniversalSingularityWrapper("haderoth", "blockHaderoth", 72, 0x77341E, 0x592817, false),
-						new UniversalSingularityWrapper("quicksilver", "blockQuicksilver", 79, 0x7CD3C7, 0x2B8073, false),
-						new UniversalSingularityWrapper("tartarite", "blockTartarite", 83, 0xAE3400, 0x792400, false),
+						new UniversalSingularityWrapper("blackSteel", "blockBlackSteel", 70, 0x395679, 0x314966),
+						new UniversalSingularityWrapper("celenegil", "blockCelenegil", 81, 0x94CC48, 0x649128),
+						new UniversalSingularityWrapper("haderoth", "blockHaderoth", 72, 0x77341E, 0x592817),
+						new UniversalSingularityWrapper("quicksilver", "blockQuicksilver", 79, 0x7CD3C7, 0x2B8073),
+						new UniversalSingularityWrapper("tartarite", "blockTartarite", 83, 0xAE3400, 0x792400),
 						//Alloys - Nether
-						new UniversalSingularityWrapper("amordrine", "blockAmordrine", 68, 0xA98DB1, 0x8F6B9A, false),
-						new UniversalSingularityWrapper("inolashite", "blockInolashite", 67, 0x40AA7D, 0x338864, false),
-						new UniversalSingularityWrapper("shadowSteel", "blockShadowSteel", 77, 0x887362, 0x766354, false)
+						new UniversalSingularityWrapper("amordrine", "blockAmordrine", 68, 0xA98DB1, 0x8F6B9A),
+						new UniversalSingularityWrapper("inolashite", "blockInolashite", 67, 0x40AA7D, 0x338864),
+						new UniversalSingularityWrapper("shadowSteel", "blockShadowSteel", 77, 0x887362, 0x766354)
 						/*Important Note:
 						 * The Rest of the Metallurgy Singularities (That are Non-Utility and Non-Alloy) Can be Found if AOBD Singularities is Used.
 						 * For the Other Metallurgy Singularities to be Added, Use AOBD Singularities, with Metallurgy.
@@ -141,7 +135,7 @@ public class CommonProxy
 				)),
 				// Redstone Arsenal Singularities
 				new UniversalSingularity("redstoneArsenal", Arrays.asList(
-						new UniversalSingularityWrapper("electrumFlux", "blockElectrumFlux", 77, 0xD0B64D, 0xA40606, false),
+						new UniversalSingularityWrapper("electrumFlux", "blockElectrumFlux", 77, 0xD0B64D, 0xA40606),
 						new UniversalSingularityWrapper("crystalFlux", "blockCrystalFlux", 76, 0xFE333A, 0xE8111A)
 				)),
 				// Tinkers' Construct Singularities
@@ -151,25 +145,24 @@ public class CommonProxy
 						new UniversalSingularityWrapper("ardite", "blockArdite", 86, 0xD24900, 0x960000),
 						new UniversalSingularityWrapper("cobalt", "blockCobalt", 67, 0x2376DD, 0x023C9B),
 						new UniversalSingularityWrapper("ender", "blockEnder", 74, 0x00B293, 0x00927C),
-						new UniversalSingularityWrapper("glue", "blockGlue", 85, 0xDBD0D0, 0xCEBFBF, false),
+						new UniversalSingularityWrapper("glue", "blockGlue", 85, 0xDBD0D0, 0xCEBFBF),
 						new UniversalSingularityWrapper("manyullyn", "blockManyullyn", 73, 0xA97DE0, 0x926AC3)
 				))
 				/*Removed
+				 * new UniversalSingularityWrapper("salt", "blockSalt", 87, 0xEFEFEF, 0xDDE7E8)
 				 *new UniversalSingularityWrapper("red_aurum", "blockRed_aurum", 78, 0xFF4809, 0xFF3D09)
 				 */
 		);
 		final Configuration config = new Configuration(new File("." + separatorChar + "config" + separatorChar + "UniversalSingularities.cfg"));
-		hideDisabledSingularities = config.get(Configuration.CATEGORY_GENERAL, "hideDisabledSingularitiesInNei", true).getBoolean() && Loader.isModLoaded("NotEnoughItems");
 		universalSingularities.forEach(universalSingularity -> {
 			if (config.get(universalSingularity.name, "enabled", true).getBoolean()) {
-				for (int i = 0; i < universalSingularity.singularities.size(); i++) {
-					final UniversalSingularityWrapper universalSingularityWrapper = universalSingularity.singularities.get(i);
-					if (config.get(universalSingularity.name, universalSingularityWrapper.name, universalSingularityWrapper.enabled).getBoolean()) {
+				universalSingularity.singularities.forEach(universalSingularityWrapper -> {
+					if (config.get(universalSingularity.name, universalSingularityWrapper.name, true).getBoolean()) {
 						if (!allowed.containsKey(universalSingularity.name))
 							allowed.put(universalSingularity.name, new HashSet<>());
 						allowed.get(universalSingularity.name).add(universalSingularityWrapper.name);
 					}
-				}
+				});
 				singularities.add(new UniversalSingularityItem(universalSingularity.name, universalSingularity.singularities));
 			}
 		});
@@ -195,15 +188,13 @@ public class CommonProxy
 		singularities.forEach(singularity -> {
 			for (int i = 0; i < singularity.universalSingularities.size(); i++) {
 				final UniversalSingularityWrapper universalSingularityWrapper = singularity.universalSingularities.get(i);
-				if (allowed.containsKey(singularity.name) && allowed.get(singularity.name).contains(universalSingularityWrapper.name)) {
+				if (allowed.get(singularity.name).contains(universalSingularityWrapper.name)) {
 					final List<ItemStack> oreList = OreDictionary.getOres(universalSingularityWrapper.oreName, false);
 					if (oreList != null && !oreList.isEmpty()) {
 						CompressorManager.addOreRecipe(new ItemStack(singularity, 1, i), universalSingularityWrapper.recipeBaseValue, universalSingularityWrapper.oreName);
 						Grinder.catalyst.getInput().add(new ItemStack(singularity, 1, i));
-					} else if (hideDisabledSingularities)
-						NEI.hide(new ItemStack(singularity, 1, i));
-				} else if (hideDisabledSingularities)
-					NEI.hide(new ItemStack(singularity, 1, i));
+					}
+				}
 			}
 		});
 	}
