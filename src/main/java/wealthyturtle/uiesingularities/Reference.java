a0f7cb6ff404e9b122c8bd192d9814bcb6168ca4
Wealthyturtle <limbennett@yahoo.com.sg>
2016-11-13 00:01:45 +0800

    Version 6 Commit

--- src/main/java/wealthyturtle/uiesingularities/Reference.java
+++ src/main/java/wealthyturtle/uiesingularities/Reference.java
@@ -4,7 +4,7 @@ public final class Reference
 {
 	public static final String MOD_ID = "universalsingularities";
 	public static final String MOD_NAME = "UniversalSingularities";
-	public static final String MOD_VERSION = "1.7.10-5.0";
+	public static final String MOD_VERSION = "1.7.10-6.0";
 	public static final String MC_VERSION = "[1.7.10]";
 	static final String COMMON_PROXY = "wealthyturtle.uiesingularities.proxy.CommonProxy";
 	static final String CLIENT_PROXY = "wealthyturtle.uiesingularities.proxy.ClientProxy";
